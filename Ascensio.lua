ac643315d3a1212ea305f7607edff2b2619006a4
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2025-04-08 13:58:11 -0400

    Added High-Five Exotic: Handromada

--- Ascensio.lua
+++ Ascensio.lua
@@ -43,6 +43,71 @@ SMODS.Atlas {
 	py = 95
 }
 
+SMODS.Atlas {
+	key = "trio",
+	path = "trio.png",
+	px = 71,
+	py = 95
+}
+
+SMODS.Atlas {
+	key = "high_five",
+	path = "high_five.png",
+	px = 71,
+	py = 95
+}
+
+--[[SMODS.Atlas {
+	key = "ascension",
+	path = "ascension.png",
+	px = 67,
+	py = 95
+}
+
+----------Defining Consumables------------------
+
+SMODS.Consumable {
+	key = "ascension",
+	set = "Spectral",
+	atlas = 'ascension',
+	pos = { x = 0, y = 0 },
+	soul_pos = { x = 2, y = 0, extra = { x = 1, y = 0 } },
+	cost = 4,
+	hidden = true,
+	config = {},
+	can_use = function(self, card)
+		if #G.jokers.highlighted == 1 and powerupable[G.jokers.highlighted[1].config.center.key] then
+				return true
+		end
+	end,
+	use = function(self, card, area, copier)
+		SMODS.add_card({key = powerupable[G.jokers.highlighted[1].config.center.key]})
+		G.jokers.highlighted[1]:remove()
+	end,
+	in_pool = function()
+		if G and G.jokers and G.jokers.cards then
+			for k, v in ipairs(G.jokers.cards) do
+				if powerupable[v.config.center.key] then
+					return true 
+				end
+			end
+		end
+		return false
+	end,
+	cry_credits = {
+		idea = {
+			"MarioFan597",
+		},
+		art = {
+			"MarioFan597",
+		},
+		code = {
+			"Inspector_B",
+		},
+	},
+}]]
+
+
 ----------Defining Jokers------------------
 SMODS.Joker {
 	key = 'jimbo',
@@ -152,6 +217,42 @@ SMODS.Joker {
 }
 
 SMODS.Joker {
+	key = 'trio',
+	config = { extra = {power = 3} },
+	rarity = "cry_exotic",
+	atlas =  'trio',
+	blueprint_compat = true,
+	pos = { x = 0, y = 0 },
+	soul_pos = { x = 2, y = 0, extra = { x = 1, y = 0 } },
+	cost = 50,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card and card.ability.extra.power} }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			if context.poker_hands ~= nil and next(context.poker_hands["Three of a Kind"]) then
+				return {
+					message = localize({ type = "variable", key = "a_powmult", vars = { card.ability.extra.power} }),
+					Emult_mod = card.ability.extra.power,
+					colour = G.C.DARK_EDITION,
+				}
+			end
+		end
+	end,
+    cry_credits = {
+			idea = {
+				"MarioFan597"
+			},
+			art = {
+				"MarioFan597"
+			},
+			code = {
+				"MarioFan597"
+			}
+		},
+}
+
+SMODS.Joker {
 	key = 'seltzer',
 	config = { extra = {retriggers = 1, played_hands = 10, goal_hands = 10}},
 	rarity = "cry_exotic",
@@ -194,7 +295,7 @@ SMODS.Joker {
 
 SMODS.Joker {
 	key = 'midas',
-	config = { extra = {power = 1.05} },
+	config = { extra = {power = 1.1} },
 	rarity = "cry_exotic",
 	atlas =  'midas',
 	blueprint_compat = true,
@@ -221,7 +322,7 @@ SMODS.Joker {
 					_card:set_ability(G.P_CENTERS[enhancement], nil, true)
 				end
 				local enhancement = "cry_golden"
-				if _card.ability.effect ~= "Golden Card" then
+				if _card.ability.effect ~= "Golden" then
 					_card:set_edition({cry_gold = true})
 				end
 				G.E_MANAGER:add_event(Event({
@@ -271,5 +372,79 @@ SMODS.Joker {
 		},
 }
 
+SMODS.Joker {
+	key = 'high_five',
+	config = { extra = {power = 1.1} },
+	rarity = "cry_exotic",
+	atlas =  'high_five',
+	blueprint_compat = false,
+	pos = { x = 0, y = 0 },
+	soul_pos = { x = 2, y = 0, extra = { x = 1, y = 0 } },
+	cost = 50,
+	loc_vars = function(self, info_queue, card)
+		info_queue[#info_queue + 1] = G.P_CENTERS.e_cry_astral
+		return { vars = { card and card.ability.extra.power} }
+	end,
+	calculate = function(self, card, context)
+		if
+			context.cardarea == G.jokers
+			and context.before
+			and not context.blueprint_card
+			and not context.retrigger_joker
+		then
+			local five_count = 0
+			for i = 1, #context.scoring_hand do
+				local _card = context.scoring_hand[i]
+				local rank = _card:get_id()
+				if rank == 5 then
+						five_count = five_count + 1
+				end
+			end
+
+			if five_count > 0 then
+				for i = 1, #context.scoring_hand do
+				local _card = context.scoring_hand[i]
+				converted = true
+				local _card = context.scoring_hand[i]
+					if _card:get_id() ~= 5 and not SMODS.has_no_rank(_card) then
+						G.E_MANAGER:add_event(Event({
+							func = function()
+								assert(SMODS.change_base(_card, _, "5"))
+								_card:juice_up()
+								return true
+							end,
+						}))
+					end
+					local enhancement = "cry_astral"
+					if _card.ability.effect ~= "Astral" then
+						_card:set_edition({cry_astral = true})
+					end
+					G.E_MANAGER:add_event(Event({
+						delay = 0.6,
+						func = function()	
+							_card:juice_up()
+							play_sound("tarot1")
+							return true
+						end,
+					}))
+				end
+				if converted then
+				return {message = 'I Wish', colour = G.C.PURPLE,}
+				end
+			end
+		end
+	end,
+    cry_credits = {
+			idea = {
+				"MarioFan597"
+			},
+			art = {
+				"MarioFan597"
+			},
+			code = {
+				"MarioFan597"
+			}
+		},
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -20,12 +20,20 @@ return {
                 "a {C:attention}Pair{}"
 		    	}
 		    },
+            j_asc_trio = {
+            name = 'Sum Trio',
+            text = {
+                "{X:dark_edition,C:white}^#1#{} Mult if played", 
+                "hand contains",
+                "a {C:attention}Three of a Kind{}"
+                }
+            },
 		    j_asc_seltzer = {
             name = 'Bulla Aquae',
 		    text = {
-                "Retriggers all cards played {C:attention}#1#{} time(s).", 
+                "Retriggers all cards played {C:attention}#1#{} time(s)", 
                 "Increase number of retriggers",
-                "after {C:attention}#3#{} hands.",
+                "after {C:attention}#3#{} hands",
                 "{C:inactive} (Hands until upgrade: {C:attention}#2#{C:inactive})"
 		    	}
 		    },
@@ -33,15 +41,33 @@ return {
             name = 'Rex Midas',
 		    text = {
                 "All played cards become", 
-                "{C:attention}Gold{} and {C:attention}Golden{} when scored.",
+                "{C:attention}Gold{} and {C:attention}Golden{} when scored,",
                 "{C:attention}Gold{} cards held in hand",
                 "give {X:dark_edition,C:white}^#1#{} Mult"
 		    	}
 		    },
+             j_asc_high_five = {
+            name = 'Handromada',
+            text = {
+                "Before scoring, if played hand", 
+                " contains a scoring {C:attention}5{},",
+                "convert {C:attention}all{} scored cards to {C:attention}5{}s",
+                "They also become {C:dark_edition}Astral"
+                }
+            },
         },
         Other={},
         Planet={},
-        Spectral={},
+        Spectral={
+            c_asc_ascension = {
+                name = "Ascension",
+                text = {
+                    "Transforms viable {C:attention}Jokers{} to",
+                    "their{C:cry_exotic,E:1} Exotic{} counterpart,",
+                    "destroy all other {C:attention}Jokers{}",
+                },
+            },
+        },
 		Code={},
         Stake={},
         Tag={},
